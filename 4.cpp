c63c51581c507719f2a9903b81fb498a018b067c
Robin Vierich <robin@uken.com>
2015-12-04 17:29:50 -0500

    working p4

--- 4.cpp
+++ 4.cpp
@@ -1,44 +1,65 @@
 #include <iostream>
 #include <openssl/md5.h>
 
+
+#define MD5_DIGEST_LENGTH 16
+
 static const char hexchars[] = "0123456789abcdef";
 
+
+std::string convertDigestToHexString(unsigned char * digest) {
+  std::string result;
+
+  for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    unsigned char elem = digest[i];
+
+    char hex[3];
+    hex[0] = hexchars[elem >> 4];
+    hex[1] = hexchars[elem & 0xF];
+    hex[2] = 0;
+
+    result.append(hex);
+  }
+
+  return result;
+}
+
+
+
+bool hasFiveZeroes(unsigned char * digest) {
+  unsigned char testNumber[16] = {0xFF, 0xFF, 0xF0, 0x00,
+                                  0x00, 0x00, 0x00, 0x00,
+                                  0x00, 0x00, 0x00, 0x00,
+                                  0x00, 0x00, 0x00, 0x00};
+
+  unsigned char andResult = 0;
+  for (int j = 0; j < MD5_DIGEST_LENGTH; j++) {
+    andResult = testNumber[j] & digest[j];
+    if (andResult) return false;
+  }
+
+  return true;
+}
+
 int main(void) {
-
-  unsigned int i = 0;
-
-  std::string key = "abcdef";
+  std::string key = "ckczppom";
   std::string data;
 
+  unsigned int i = 0;
+  bool _hasFiveZeroes = false;
+  unsigned char * digest;
 
-
-  for (int i = 0; i < 100; i++) {
-    std::string result;
-
+  while (!_hasFiveZeroes) {
     data = key + std::to_string(i);
 
-
-    unsigned char * digest;
     digest = MD5((const unsigned char *)data.c_str(), data.size(), NULL);
+    _hasFiveZeroes = hasFiveZeroes(digest);
 
-    // unsigned char * tmp = digest;
-    // std::cout << i << ":";
-
-    // while(tmp) {
-    //   std::cout << tmp++;
-    // }
-
-    for (int j = 0; j < 16; j++) {
-      unsigned char elem = digest[i];
-
-      char hex[3];
-      hex[0] = hexchars[elem >> 4];
-      hex[1] = hexchars[elem & 0xF];
-      hex[2] = 0;
-
-      result.append(hex);
-    }
-
-    std::cout << i << "    data: " << data << "    result:" << result << std::endl;
+    ++i;
   }
+
+  std::string result = convertDigestToHexString(digest);
+
+  std::cout << i-1 << "    data: " << data << "    result:" << result << std::endl;
+  std::cout << "has 5 zeroes: " << _hasFiveZeroes << std::endl;
 }
